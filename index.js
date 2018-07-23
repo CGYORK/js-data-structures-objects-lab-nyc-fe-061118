0a23c45fe29d848a8971add29c90c569035b556d
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-23 22:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
-function deleteFromDriverByKey(driver, key) {
+function deleteFromDriverByKey(newDriver, key) {
   delete newDriver[key];
   return newDriver;
 }
