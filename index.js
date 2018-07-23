c7d0e6faee51f877708ce0794a61e8ebbeb79988
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-23 22:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 let driver = {name: "Program"};
-function updateDriverWithKeyAndValue() {
-  let newDriver = {...driver}
+function updateDriverWithKeyAndValue(obj, key, value) {
+  let newDriver = {...driver};
   
 }
